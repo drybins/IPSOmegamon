9f8c6f1646573f4592b5b2d17a12fe3f54219ba3
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2023-09-04 22:49:30 +0200

    Update module.php

--- CPU/module.php
+++ CPU/module.php
@@ -36,7 +36,7 @@ declare(strict_types=1);
 			$this->SendDebug("Dierk", "CPU", 0);
 			$a = 2.1;
 			$b = 28445;
-			//$this->SetValue($this->GetIDForIdent("CPU_0"),2.00);
-			SetValue($b,$a);
+			SetValue($this->GetIDForIdent("CPU_0"),2.20);
+			//SetValue($b,$a);
 		}
 	}
